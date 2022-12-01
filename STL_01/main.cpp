f8d1e535b5e8af96d0258f1b7b9ac4f24fdc0cb9
Furkan Karagoz <fkaragoz14@gmail.com>
2022-12-02 01:31:20 +0300

    STL_01 initial commit.

---
+++ STL_01/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// Generic types : Templates
+
+template <typename T>
+
+int size_in_bits(const T& a)
+{
+	return sizeof(a) * 8;
+}
+
+int main(void)
+{
+	cout << size_in_bits(21) << endl;	// int constant
+	cout << size_in_bits('f') << endl;	// char constant
+	cout << size_in_bits(32.1f) << endl;	// float constant
+	cout << size_in_bits(32.1) << endl;	// double constant
+	return 0;
+}
