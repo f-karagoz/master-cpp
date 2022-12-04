f3607b13bf1aa4467caaff8d9258ac2350a7bf1a
Furkan Karagoz <fkaragoz14@gmail.com>
2022-12-05 02:11:57 +0300

    Multiset, unordered_set, and unordered_multiset study.

---
+++ STL_12/main.cpp
@@ -0,0 +1,61 @@
+/*
+* Title: Multiset
+* Author: Furkan Karagoz
+* Instructor: Eduardo Corpeno
+* Date: 5 / 12 / 2022
+*/
+
+#include <iostream>
+#include <set>
+#include <unordered_set>
+using namespace std;
+
+// associative container
+// addresses elements by content rather than address
+// duplicates are allowed in multi sets
+
+int main(void)
+{
+	//set<int> this_set;
+	//multiset<int> this_set;
+	//unordered_set<int> this_set;
+	unordered_multiset<int> this_set;
+	int temp = 0;
+
+	/* 1 - Insert elements */
+
+	cout << "Inserting..." << endl;
+	while (temp >= 0)
+	{
+		cout << "Enter number: ";
+		cin >> temp;
+		if (temp >= 0)
+			this_set.insert(temp);
+	}
+
+	/* Print out */
+	cout << "{ ";
+	for (auto it = this_set.begin(); it != this_set.end(); ++it)
+		cout << *it << " ";
+	cout << "}" << endl;
+
+	/* 2 - Remove by element */
+
+	temp = 0;
+	cout << "Removing..." << endl;
+	while (temp >= 0)
+	{
+		cout << "Enter number: ";
+		cin >> temp;
+		if (temp >= 0)
+			this_set.erase(temp);
+	}
+
+	/* Print out */
+	cout << "{ ";
+	for (auto it = this_set.begin(); it != this_set.end(); ++it)
+		cout << *it << " ";
+	cout << "}" << endl;
+
+	return 0;
+}
