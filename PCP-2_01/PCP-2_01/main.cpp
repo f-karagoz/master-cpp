bb335ea524361487cf0da8c1be3331f32a89d5f9
Furkan Karagoz <fkaragoz14@gmail.com>
2023-02-02 01:27:38 +0300

    gitignore updated.

---
+++ PCP-2_01/PCP-2_01/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PCP-2_01
+//
+//  Created by Furkan Karagöz on 2.02.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
