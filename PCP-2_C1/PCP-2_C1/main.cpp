7fd8231a08684243be428c8b42e4cb5f65efb728
Furkan Karagoz <fkaragoz14@gmail.com>
2023-03-02 02:04:10 +0300

    Challenge: Matrix multiplication.

--- PCP-2_C1/PCP-2_C1/main.cpp
+++ PCP-2_C1/PCP-2_C1/main.cpp
@@ -30,15 +30,18 @@ void sequential_matrix_multiply(long ** A, size_t num_rows_a, size_t num_cols_a,
     }
 }
 
-void line_product(long ** A, size_t num_rows_a, size_t num_cols_a,
+void chunk_work(long ** A, size_t num_rows_a, size_t num_cols_a,
                   long ** B, size_t num_rows_b, size_t num_cols_b,
-                  long ** C, size_t line) {
+                  long ** C, size_t start_row, size_t end_row) {
     
-    for (size_t j=0; j<num_cols_b; j++)
+    for (size_t i=start_row; i<end_row; i++)
     {
-        C[line][j] = 0;
-        for (size_t k=0; k<num_cols_a; k++)
-            C[line][j] += A[line][k] * B[k][j];
+        for (size_t j=0; j<num_cols_b; j++) {
+            C[i][j] = 0; // initialize result cell to zero
+            for (size_t k=0; k<num_cols_a; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
     }
 }
 
@@ -48,31 +51,33 @@ void parallel_matrix_multiply(long ** A, size_t num_rows_a, size_t num_cols_a,
                         long ** C) {
     
     size_t n_threads = std::thread::hardware_concurrency();
+    size_t n_lines_per_thread = ceil( (float)num_rows_a / n_threads );
     
-    std::thread line_calculators[n_threads];
-    for (size_t i=0; i<num_rows_a; i++)
+    std::thread chunk_workers[n_threads];
+    
+    for (size_t i=0; i<n_threads; i++)
     {
-        for (size_t n=0; n<n_threads && i<num_rows_a; n++, i++)
-        {
-            line_calculators[n] = std::thread(line_product,
-                                              A, num_rows_a, num_cols_a,
-                                              B, num_rows_b, num_cols_b,
-                                              C, i);
-        }
-        for (auto& t: line_calculators)
-        {
-            t.join();
-        }
+        size_t chunk_start = i * n_lines_per_thread;
+        size_t chunk_end = std::min( (i+1) * n_lines_per_thread, num_rows_a);
+        
+        chunk_workers[i] = std::thread(chunk_work,
+                                        A, num_rows_a, num_cols_a,
+                                        B, num_rows_b, num_cols_b,
+                                        C, chunk_start, chunk_end);
+    }
+    for (auto& t: chunk_workers)
+    {
+        t.join();
     }
 }
 
 
 int main() {
     const int NUM_EVAL_RUNS = 3;
-    const size_t NUM_ROWS_A = 1000;
-    const size_t NUM_COLS_A = 1000;
+    const size_t NUM_ROWS_A = 100;
+    const size_t NUM_COLS_A = 100;
     const size_t NUM_ROWS_B = NUM_COLS_A;
-    const size_t NUM_COLS_B = 1000;
+    const size_t NUM_COLS_B = 100;
 
     // intialize A with values in range 1 to 100
     long ** A = (long **)malloc(NUM_ROWS_A * sizeof(long *));
@@ -151,3 +156,63 @@ int main() {
     printf("Speedup: %.2f\n", sequential_time/parallel_time);
     printf("Efficiency %.2f%%\n", 100*(sequential_time/parallel_time)/std::thread::hardware_concurrency());
 }
+
+/* Result-1:
+ const int NUM_EVAL_RUNS = 3;
+ const size_t NUM_ROWS_A = 1000;
+ const size_t NUM_COLS_A = 1000;
+ const size_t NUM_ROWS_B = NUM_COLS_A;
+ const size_t NUM_COLS_B = 1000;
+ -----------------------------------
+ Evaluating Sequential Implementation...
+ Evaluating Parallel Implementation...
+ Average Sequential Time: 11133.27 ms
+   Average Parallel Time: 2319.12 ms
+ Speedup: 4.80
+ Efficiency 60.01%
+ */
+
+/* Result-2:
+ const int NUM_EVAL_RUNS = 3;
+ const size_t NUM_ROWS_A = 10000;
+ const size_t NUM_COLS_A = 1000;
+ const size_t NUM_ROWS_B = NUM_COLS_A;
+ const size_t NUM_COLS_B = 1000;
+ -----------------------------------
+ Evaluating Sequential Implementation...
+ Evaluating Parallel Implementation...
+ Average Sequential Time: 113457.73 ms
+   Average Parallel Time: 29320.77 ms
+ Speedup: 3.87
+ Efficiency 48.37%
+ */
+
+/* Result-3:
+ const int NUM_EVAL_RUNS = 3;
+ const size_t NUM_ROWS_A = 1000;
+ const size_t NUM_COLS_A = 10000;
+ const size_t NUM_ROWS_B = NUM_COLS_A;
+ const size_t NUM_COLS_B = 1000;
+ -----------------------------------
+ Evaluating Sequential Implementation...
+ Evaluating Parallel Implementation...
+ Average Sequential Time: 194606.47 ms
+   Average Parallel Time: 44937.40 ms
+ Speedup: 4.33
+ Efficiency 54.13%
+ */
+
+/* Result-4:
+ const int NUM_EVAL_RUNS = 3;
+ const size_t NUM_ROWS_A = 100;
+ const size_t NUM_COLS_A = 100;
+ const size_t NUM_ROWS_B = NUM_COLS_A;
+ const size_t NUM_COLS_B = 100;
+ -----------------------------------
+ Evaluating Sequential Implementation...
+ Evaluating Parallel Implementation...
+ Average Sequential Time: 3.19 ms
+   Average Parallel Time: 1.08 ms
+ Speedup: 2.96
+ Efficiency 36.99%
+ */
